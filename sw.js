d286eed336dc36dfecedfd9bb35550d2becc7736
leia-sales-ai <bjoern@ar-giri.com>
2026-09-19 15:51:33 +0200

    Super Johanna: sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'super-johanna-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if(req.method !== 'GET') return;
+  e.respondWith(caches.match(req).then(hit => hit || fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
